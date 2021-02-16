8c868ad1b69fdcf8333c3fc7af6837266bca5f10
James <JamesWhite1911@protonmail.com>
2021-02-16 02:55:12 -0500

    mvp complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,42 +1,66 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+//import our actions
+import { APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, SET_MEMORY, ADD_MEMORY, CLEAR_MEMORY } from './../actions';
 
+//initial state
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
-    switch(operation) {
-        case("+"):
+    switch (operation) {
+        case ("+"):
             return num1 + num2;
-        case("*"):
+        case ("*"):
             return num1 * num2;
-        case("-"):
+        case ("-"):
             return num1 - num2;
+        default:
+            return;
     }
 }
 
+//reducer switch
 const reducer = (state, action) => {
-    switch(action.type) {
-        case(ADD_ONE):
-            return({
-                ...state,
-                total: state.total + 1
-            });
+    switch (action.type) {
 
-        case(APPLY_NUMBER):
-            return ({ 
-                ...state, 
+        case (APPLY_NUMBER):
+            return ({
+                ...state,
                 total: calculateResult(state.total, action.payload, state.operation)
             });
-        
-        case(CHANGE_OPERATION):
+
+        case (CHANGE_OPERATION):
             return ({
                 ...state,
                 operation: action.payload
             });
-            
+
+        case (CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: 0
+            });
+
+        case (SET_MEMORY):
+            return ({
+                ...state,
+                memory: state.total
+            });
+
+        case (ADD_MEMORY):
+            return ({
+                ...state,
+                total: state.total + state.memory
+            });
+
+        case (CLEAR_MEMORY):
+            return ({
+                ...state,
+                memory: 0
+            });
+
         default:
             return state;
     }
